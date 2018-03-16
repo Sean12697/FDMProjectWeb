7dbf3070c9413a52bf07e9a89d7f76c45e316ee9
Sean O'Mahoney <seandavidomahoney@gmail.com>
2018-03-16 14:43:44 +0000

    Nearly

--- FDMProjectWeb/wwwroot/script.js
+++ FDMProjectWeb/wwwroot/script.js
@@ -52,9 +52,9 @@ function init() {
         //console.log(legends);
         //console.log(unique);
         
-        doShit(type, label, legends, unique, label2, legends2, unique2, []);
+        main(type, label, legends, unique, label2, legends2, unique2, []);
 
-        //doShit(document.getElementById("type").value, data.map(a => a[document.getElementById("legend").value]), data.map(a => a[document.getElementById("axisX").value]), data.map(a => a[document.getElementById("axisY").value]));
+        //main(document.getElementById("type").value, data.map(a => a[document.getElementById("legend").value]), data.map(a => a[document.getElementById("axisX").value]), data.map(a => a[document.getElementById("axisY").value]));
     });
 }
 
@@ -62,6 +62,112 @@ document.getElementById("type").addEventListener("change", init);
 document.getElementById("dataOne").addEventListener("change", init);
 document.getElementById("dataTwo").addEventListener("change", init);
 document.getElementById("reportBtn1").addEventListener("click", timeOnSite);
+document.getElementById("reportBtn2").addEventListener("click", universityPlacement);
+document.getElementById("reportBtn3").addEventListener("click", subjectJob);
+document.getElementById("reportBtn4").addEventListener("click", classJob);
+
+function subjectJob() {
+    loadJSON(function (response) {
+        var data = JSON.parse(response);
+        subjectJobChart(data);
+    });
+}
+
+function subjectJobChart(data) {
+
+}
+
+function universityPlacement() {
+    loadJSON(function (response) {
+        var data = JSON.parse(response);
+        universityPlacementChart(data);
+    });
+}
+
+function universityPlacementChart(data) {
+
+    replaceChart();
+    var ctx = document.getElementById("myChart");
+
+    var unis = trunk(data, "University of Study");
+    var placement = trunk(data, "Operating Location Name");
+    var uniqueUnis = getUnique(unis);
+    var uniquePlacement = getUnique(placement);
+
+    var bc = [];
+    for (var i = 0; i < uniqueUnis.length; i++) bc.push('rgba(' + getRandomInt(255) + ', ' + getRandomInt(255) + ', ' + getRandomInt(255) + ', 0.2)');
+
+    var datasets = [];
+    for (var i = 0; i < uniquePlacement.length; i++) {
+
+        datasets.push({
+            label: uniquePlacement[i],
+            data: overlapArr(data, uniquePlacement[i]), // tots
+            backgroundColor: bc,
+            borderWidth: 2
+        });
+    }
+    
+    console.log(datasets);
+
+    
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: uniqueUnis,
+            datasets: datasets
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    stacked: true,
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }],
+                xAxes: [{
+                    stacked: true,
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+
+            }
+        }
+    });
+}
+
+
+// return overlapped, [10, 20, 10], numbers for each uni in a placement 
+function overlapArr(data, placementLocation) {
+    var unis = trunk(data, "University of Study");
+    var placements = trunk(data, placementLocation);
+    var uniqueUnis = getUnique(unis);
+
+    var nums = [];
+    for (var i = 0; i < uniqueUnis.length; i++) nums.push(0);
+
+    for (var i = 0; i < unis.length; i++) {
+        for (var j = 0; j < placements.length; j++) {
+            for (var t = 0; t < uniqueUnis.length; t++) {
+                if (uniqueUnis[t] == unis[i] && placements[j] == placementLocation) {
+                    nums[t]++;
+                }
+            }
+        }
+    }
+}
+
+function classJob() {
+    loadJSON(function (response) {
+        var data = JSON.parse(response);
+        classJobChart(data);
+    });
+}
+
+function classJobChart(data) {
+
+}
 
 function returnLengthOfTimeArr() {
     loadJSON(function (response) {
@@ -93,9 +199,13 @@ function timeOnSite() {
     });
 }
 
-function timeOnSiteChart(data, times) {
+function replaceChart() {
     document.getElementById("chartDisplay").innerHTML = "";
     document.getElementById("chartDisplay").insertAdjacentHTML('beforeend', '<canvas id="myChart"></canvas>');
+}
+
+function timeOnSiteChart(data, times) {
+    replaceChart();
     var ctx = document.getElementById("myChart").getContext('2d');
     
     var degrees = trunk(data, "Degree Subject");
@@ -143,14 +253,13 @@ function timeOnSiteChart(data, times) {
     });
 }
 
-function doShit(type, label, legend, dataOne, label2, legend2, dataTwo) {
+function main(type, label, legend, dataOne, label2, legend2, dataTwo) {
 
     var bc = [];
     for (var i = 0; i < dataOne.length; i++) bc.push('rgba(' + getRandomInt(255) + ', ' + getRandomInt(255) + ', ' + getRandomInt(255) +', 0.2)');
     for (var i = 0; i < dataTwo.length; i++) bc.push('rgba(' + getRandomInt(255) + ', ' + getRandomInt(255) + ', ' + getRandomInt(255) + ', 0.2)');
-
-    document.getElementById("chartDisplay").innerHTML = "";
-    document.getElementById("chartDisplay").insertAdjacentHTML('beforeend', '<canvas id="myChart"></canvas>');
+    
+    replaceChart();
     var ctx = document.getElementById("myChart").getContext('2d');
 
     var myChart = new Chart(ctx, {
@@ -159,7 +268,6 @@ function doShit(type, label, legend, dataOne, label2, legend2, dataTwo) {
             //labels: legend,
             labels: legend2,
             datasets: [{
-
                 label: label, 
                 data: dataOne,
                 backgroundColor: bc,
